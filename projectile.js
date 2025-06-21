02d7842f58a75af468208d2c17c281387ba4736f
Shawn Altman <shawn@dreamspectrum.com>
2025-06-20 19:57:49 -0700

    Add missing Alien and Projectile classes

---
+++ projectile.js
@@ -0,0 +1,21 @@
+import { Entity } from './entityBaseClass.js';
+
+export class Projectile extends Entity {
+  constructor(x, y, options = {}) {
+    super(x, y, options.width || 4, options.height || 10);
+    this.vy = options.speed || -300;
+    this.config = options;
+  }
+
+  update(dt) {
+    this.y += this.vy * dt;
+    if (this.y + this.height < 0) {
+      this.alive = false;
+    }
+  }
+
+  draw(ctx) {
+    ctx.fillStyle = this.config.color || 'white';
+    ctx.fillRect(this.x - this.width / 2, this.y, this.width, this.height);
+  }
+}
